c3f639f3986f5099b5d9bfed439d42fd6678f295
adityalesmana <38103892+adityalesmana@users.noreply.github.com>
2018-05-06 02:21:52 -0700

    Constructur - sama dengan nama class

--- src/praktek02adt/persegi.java
+++ src/praktek02adt/persegi.java
@@ -8,27 +8,7 @@ public class persegi {
         tinggi=6;   
     }
 
-    public persegi(double alas, double tinggi) {
-        this.alas = alas;
-        this.tinggi = tinggi;
-    }
-    
-    
-    
     void cetakInfo() {
         System.out.println("==================");
-        System.out.println("alas   :"+alas);
-        System.out.println("tinggi :"+tinggi);
-        System.out.println("==================");
     }
-    
-    double hitungKeliling() {
-        double keliling;
-        keliling=alas*tinggi;
-        return keliling;
-    }
-    
-    void cetakKeliling(){
-        System.out.println("Kelilingnya adalah : "+hitungKeliling());
-    }
-}
+}    
