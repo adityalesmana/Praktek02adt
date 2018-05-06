81fc1c3c416435cb992eb45c515680993785459a
adityalesmana <38103892+adityalesmana@users.noreply.github.com>
2018-05-06 02:04:08 -0700

    Mengisi nilai atribute

--- src/praktek02adt/persegiAksi.java
+++ src/praktek02adt/persegiAksi.java
@@ -3,5 +3,8 @@ package praktek02adt;
 public class persegiAksi {
     public static void main(String[] args) {
         persegi rl = new persegi();
+        
+        rl.alas   = 7;
+        rl.tinggi = 9;
 }
 }
