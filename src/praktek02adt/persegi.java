90a0bcc2cf082b5c87d0c7173912d4315edb8f83
adityalesmana <38103892+adityalesmana@users.noreply.github.com>
2018-05-06 02:01:56 -0700

    Instance Object

--- src/praktek02adt/persegi.java
+++ src/praktek02adt/persegi.java
@@ -3,10 +3,32 @@ public class persegi {
     double alas;
     double tinggi;
 
+    public persegi() {
+        alas=3;
+        tinggi=6;   
+    }
+
+    public persegi(double alas, double tinggi) {
+        this.alas = alas;
+        this.tinggi = tinggi;
+    }
+    
+    
+    
     void cetakInfo() {
         System.out.println("==================");
         System.out.println("alas   :"+alas);
         System.out.println("tinggi :"+tinggi);
         System.out.println("==================");
+    }
+    
+    double hitungKeliling() {
+        double keliling;
+        keliling=alas*tinggi;
+        return keliling;
+    }
+    
+    void cetakKeliling(){
+        System.out.println("Kelilingnya adalah : "+hitungKeliling());
     }
 }
--- src/praktek02adt/persegiAksi.java
+++ src/praktek02adt/persegiAksi.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek02adt;
 
-/**
- *
- * @author LENOVO
- */
 public class persegiAksi {
-    
+    public static void main(String[] args) {
+        persegi rl = new persegi();
+}
 }
