6aed3cdd8cfdb762ab58741efc9bb5a681895464
adityalesmana <38103892+adityalesmana@users.noreply.github.com>
2018-05-06 01:55:24 -0700

    Membuat Attribute dan Behavior

--- src/praktek02adt/persegi.java
+++ src/praktek02adt/persegi.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek02adt;
-
-/**
- *
- * @author LENOVO
- */
 public class persegi {
-    
+    double alas;
+    double tinggi;
+
+    void cetakInfo() {
+        System.out.println("==================");
+        System.out.println("alas   :"+alas);
+        System.out.println("tinggi :"+tinggi);
+        System.out.println("==================");
+    }
 }
